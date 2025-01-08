c0ddfd89b3a85672b83030d3351211ae76bb9c75
Ayhem <Ayhemalras@outlook.de>
2025-01-08 14:28:07 +0100

    w

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,37 +3,37 @@ import './Projects.css';
 const projects = [
   {
     title: "Umzug-Entrümpelung",
-    description: 'A project for managing and organizing moves and cleanouts.',
+    description: '',
     link: 'https://umzug-projekt.vercel.app/',
-    image: '/src/public/images/umzug.png'
+    image: '/images/umzug.png'
   },
   {
     title: "E-Commerce",
-    description: 'An e-commerce platform built with modern web technologies.',
+    description: '',
     link: 'https://github.com/Ayhem-Alras/Ampes',
     image: '/images/E.png'
   },
   {
     title: 'Movie-Search',
-    description: 'A movie search application with a list of favorite movies.',
+    description: '',
     link: 'https://favorite-movies-list-mqtx.vercel.app/',
     image: '/images/Screenshot 665.png'
   },
   {
     title: 'Wetter-App',
-    description: 'A weather application providing current weather updates.',
+    description: '',
     link: 'https://weather-app-eta-five-31.vercel.app',
     image: '/images/Screenshot 2024-06-27 123554.png'
   },
   {
     title: 'To-do-List',
-    description: 'A simple and efficient to-do list application.',
+    description: '',
     link: 'https://to-do-list-bh17488zn-ayhem-alras-projects.vercel.app/',
     image: '/images/Screenshot 2024-06-27 131658.png'
   },
   {
-    title: 'Taschenrechner',
-    description: 'A calculator application for basic arithmetic operations.',
+    title: 'Taschenrschener',
+    description: '',
     link: 'https://taschenrechner-z8z8.vercel.app/',
     image: '/images/Screenshot 2024-06-27 133058.png'
   }
@@ -46,12 +46,7 @@ const Projects = () => {
       <div className="projects-list">
         {projects.map((project, index) => (
           <div key={index} className="project-item">
-            <img 
-              src={project.image} 
-              alt={project.title} 
-              className="project-image" 
-              onError={(e) => e.target.src = '/images/fallback.png'} // Fallback image
-            />
+            <img src={project.image} alt={project.title} className="project-image" />
             <h3>{project.title}</h3>
             <p>{project.description}</p>
             <a href={project.link} target="_blank" rel="noopener noreferrer">Projekt zeigen</a>
