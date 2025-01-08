182e6bc2f57df07e0d9d63bc53a793ecefdf67eb
Ayhem <Ayhemalras@outlook.de>
2025-01-08 14:45:25 +0100

    w

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,37 +3,37 @@ import './Projects.css';
 const projects = [
   {
     title: "Umzug-Entrümpelung",
-    description: '',
+    description: 'Planung und Durchführung eines Umzugs mit gleichzeitiger Entrümpelung.',
     link: 'https://umzug-projekt.vercel.app/',
     image: '/images/umzug.png'
   },
   {
     title: "E-Commerce",
-    description: '',
+    description: 'Entwicklung einer Plattform für den Online-Verkauf von Produkten.',
     link: 'https://github.com/Ayhem-Alras/Ampes',
     image: '/images/E.png'
   },
   {
     title: 'Movie-Search',
-    description: '',
+    description: 'Eine App zum Durchsuchen und Speichern von Lieblingsfilmen.',
     link: 'https://favorite-movies-list-mqtx.vercel.app/',
     image: '/images/Screenshot 665.png'
   },
   {
     title: 'Wetter-App',
-    description: '',
+    description: 'Anzeige aktueller Wetterdaten basierend auf dem Standort des Nutzers.',
     link: 'https://weather-app-eta-five-31.vercel.app',
     image: '/images/Screenshot 2024-06-27 123554.png'
   },
   {
     title: 'To-do-List',
-    description: '',
+    description: 'Ein Tool zur Verwaltung und Organisation täglicher Aufgaben.',
     link: 'https://to-do-list-bh17488zn-ayhem-alras-projects.vercel.app/',
     image: '/images/Screenshot 2024-06-27 131658.png'
   },
   {
-    title: 'Taschenrschener',
-    description: '',
+    title: 'Taschenrechner',
+    description: 'Eine einfache und intuitive Taschenrechner-App für den täglichen Gebrauch.',
     link: 'https://taschenrechner-z8z8.vercel.app/',
     image: '/images/Screenshot 2024-06-27 133058.png'
   }
